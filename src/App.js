8375bfc28516384463f7d895375a7f7f1aaa8a26
Ferdi Mehmedov <FredericCo@mail.bg>
2024-01-04 10:58:12 +0200

    onClick function create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
 import './App.css';
 
 import Calculator from './components/calculator/Calculator';
 
 function App() {
   const [result, setResult] = useState('');
-  
+
+  const onClick = (e) => {       
+    setResult(result.concat(e.target.name))
+          
+  }
+
+
   return (
     <Calculator />
   );
