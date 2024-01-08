a1c926e3992cbbb6f0604a436db10d5d28b9b2ee
Ferdi Mehmedov <FredericCo@mail.bg>
2024-01-08 10:54:06 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import React, { useState } from 'react';
-import './App.css';
-
 import Calculator from './components/calculator/Calculator';
 
-function App() {
-  const [result, setResult] = useState('');
-
-  const onClick = (e) => {
-    setResult(result.concat(e.target.name))
-  };
-
-  const onClear = () => {
-    setResult('')
-  };
-
-  const onBack = () => {
-        setResult(result.slice(0, result.length - 1)); 
-  };
-
-  const onCalculate = () => {
-    try {
-        setResult(Number(eval(result).toString()).toFixed(15));
-    } catch (error) {
-        setResult('Error')
-    }      
-};
-
-
+function App() {  
   return (
     <Calculator />
   );
--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,6 +1,28 @@
+import React, { useState } from 'react';
 import './calculator.css';
 
 const Calculator = () => {
+    const [result, setResult] = useState('');
+
+  const onClick = (e) => {
+    setResult(result.concat(e.target.name))
+  };
+
+  const onClear = () => {
+    setResult('')
+  };
+
+  const onBack = () => {
+        setResult(result.slice(0, result.length - 1)); 
+  };
+
+  const onCalculate = () => {
+    try {
+        setResult(Number(eval(result).toString()).toFixed(15));
+    } catch (error) {
+        setResult('Error')
+    }
+};
 
 
     return (
