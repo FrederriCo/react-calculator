fc533e25c3140f4253c86cd69c4a61c2435dfc0d
Ferdi Mehmedov <FredericCo@mail.bg>
2024-01-08 11:07:40 +0200

    add form

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -4,30 +4,33 @@ import './calculator.css';
 const Calculator = () => {
     const [result, setResult] = useState('');
 
-  const onClick = (e) => {
-    setResult(result.concat(e.target.name))
-  };
+    const onClick = (e) => {
+        setResult(result.concat(e.target.name))
+    };
 
-  const onClear = () => {
-    setResult('')
-  };
+    const onClear = () => {
+        setResult('')
+    };
 
-  const onBack = () => {
-        setResult(result.slice(0, result.length - 1)); 
-  };
+    const onBack = () => {
+        setResult(result.slice(0, result.length - 1));
+    };
 
-  const onCalculate = () => {
-    try {
-        setResult(Number(eval(result).toString()).toFixed(15));
-    } catch (error) {
-        setResult('Error')
-    }
-};
-
+    const onCalculate = () => {
+        try {
+            setResult(Number(eval(result).toString()).toFixed(15));
+        } catch (error) {
+            setResult('Error')
+        }
+    };
 
     return (
         <>
-
+            <div className='container'>
+                <form action="">
+                    <input type="text" value={result} />
+                </form>
+            </div>
         </>
     )
 }
