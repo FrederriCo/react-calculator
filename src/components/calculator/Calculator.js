3b9d0868606a4030e8362e100514e0a5f2975592
Ferdi Mehmedov <FredericCo@mail.bg>
2024-01-27 09:49:20 +0200

    button

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -46,6 +46,7 @@ const Calculator = () => {
 					<button name='2' onClick={onClick}>2</button>
 					<button name='3' onClick={onClick}>3</button>
 					<button name='+' onClick={onClick}>+</button>
+					<button name='0' onClick={onClick}>0</button>
                 </div>
 
             </div>
